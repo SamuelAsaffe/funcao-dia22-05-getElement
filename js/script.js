30c45e782cf70c0e796b3c897f17ab388c544d81
SamuelAsaffe <samuelasaffe1@gmail.com>
2023-05-25 14:58:22 -0300

    a

--- js/script.js
+++ js/script.js
@@ -6,7 +6,24 @@ function circulo() {
     alert(" A área do circulo é igual a: " + circ)
 }
 
-//   terefa 2
+//   tarefa 2 área do retangulo
+
+function retangulo() {
+    let baseret = prompt("qual o valor da base ?")
+    let alturaret = prompt("qual o valor da altura ?")
+    let arearet = alturaret * baseret
+    alert(" A área do retangulo é igual a: " + arearet)
+}
+//   tarefa 3 área do triangulo
+
+function triangulo() {
+    let basetri = prompt("qual o valor da base ?")
+    let alturatri = prompt("qual o valor da altura ?")
+    let areatri = alturatri * basetri
+    alert(" A área do triangulo é igual a: " + areatri)
+}
+
+//   terefa 4 hipotenusa
 function hipotenusa() {
     let ca = prompt("Qual o valor do cateto oposto?")
     let co = prompt("Qual o valor do cateto adjacente?")
@@ -16,6 +33,7 @@ function hipotenusa() {
 
     alert("O valor da hipotenusa é: " + hp)
 }
+//     tarefa 5  reais para dolares
 function dolares() {
     let reais = prompt("Quantos reais que você deseja trocar por dolares? ")
     let cambio = prompt("qual a contação do dolar hoje? ")
@@ -24,7 +42,7 @@ function dolares() {
     alert("Seus reais foram convertidos para " + dol + " dolares ")
 }
 
-//     tarefa 4
+//     tarefa 6 celcius para fahreheit
 function celcius() {
     let c = prompt("quantos graus celcius que é para converter para fahrenheit? ")
     let f = 32 + (c * 1.8)
@@ -33,7 +51,7 @@ function celcius() {
 }
 
 
-//    tarefa 5   media aritimética de 3 notas
+//    tarefa 7   media aritimética de 3 notas
 
 function mediaAritimetica() {
     let n1 = parseFloat(prompt(" qual o valor de 1ª nota? "))
@@ -42,10 +60,18 @@ function mediaAritimetica() {
     let media = ((n1 + n2 + n3) / 5)
     media = parseFloat(media.toFixed(2))
     alert(" O valor da média aritimética é: " + media)
-
 }
 
-//      tarefa 6 bhaskara
+
+//      tarefa 8   km/h para m/s
+function velocidade() {
+    let km = prompt(" Qual a velocidade em KM/H ? ")
+    let ms = prompt(" Qual a velocidade em m/s ? ")
+    ms=km*3.6
+    alert("a velocidade em metros por segundo é = "+ ms)
+}
+
+//      tarefa 9 bhaskara
 
 function bhaskara() {
     
@@ -55,11 +81,12 @@ function bhaskara() {
     c = parseFloat(prompt("Digite o valor de c: "));
     delta= b*b -4*a*c
     if( delta <0 ) {
-        alert("a equação não possui raizes reais.")
+
+        let nprr= alert("a equação não possui raizes reais.")
     }
     else if (delta===0) {
         r=-b/(2*a)
-        alert("A equação possui uma raiz real: " + r.toFixed(2))
+        let prr = alert("A equação possui uma raiz real: " + r.toFixed(2))
         
     }
     else {
@@ -67,10 +94,6 @@ function bhaskara() {
         x2=(-b - Math.sqrt(delta))/(2*a)
         alert("A equação possui duas raizes reais: " +x1.toFixed(2) + " e " + x2.toFixed(2))
     }
-
-
-
-
 }
   //ja posso xingar ? kkkkk
 
